c2007a1ddeb6fbaaf2660ad0f3030d58d9d3cd77
leonhma <67312077+leonhma@users.noreply.github.com>
2022-03-20 16:20:49 +0100

    a4

--- a4-Bonus/kxor.py
+++ a4-Bonus/kxor.py
@@ -3,6 +3,7 @@ import os
 from functools import reduce
 from math import factorial
 from operator import xor
+from typing import List
 
 from alive_progress import alive_bar
 
@@ -17,14 +18,13 @@ cardstack = [0] * n
 for i, line in enumerate(lines[1:][:n]):
     cardstack[i] = int(line[:m+1], 2)
 
-cardstack.sort()
 
-# O(n^k)
-with alive_bar(int(factorial(n) / (factorial(n - (k)) * factorial(k)))) as bar:
-    for comb in itertools.combinations(cardstack, k):
-        bar()
-        res = reduce(xor, comb)
-        if res in cardstack:
-            for x in list(comb)+[res]:
-                print(cardstack.index(x), bin(x)[2:].zfill(m))
-            break
+def kxor(arr: List[int], k: int) -> List[int]:
+    current = [0]
+
+    def recurse(arr: List[int], i: int) -> List[int]:
+        
+        if k > 2:
+            # recurse
+        else:
+            l, r = current[-1], k+1
--- a4-Bonus/program2.py
+++ a4-Bonus/program2.py
@@ -3,10 +3,13 @@ from functools import reduce
 from itertools import combinations
 from math import factorial
 from operator import xor
+from typing import Iterable, Tuple
 
 from alive_progress import alive_bar
 
 path = os.path.dirname(os.path.realpath(__file__))+f'/beispieldaten/stapel{2}.txt'
+
+splits = 16
 
 with open(path, 'r') as f:
     lines = f.read().split('\n')
@@ -20,19 +23,22 @@ for i, line in enumerate(lines[1:][:n]):
 
 cardstack.sort()
 
-# time complexity: O(n^(k-1)), space complexity: O(n)
-with alive_bar(int(factorial(n) / (factorial(n - (k-1)) * factorial(k-1)))) as bar:
-    for ii in combinations(range(len(cardstack))[:-2], k-1):  # ii: list of indices
-        bar()
-        goal = reduce(xor, [cardstack[i] for i in ii])
-        l, r = ii[-1]+1, len(cardstack)-1
-        while l < r:
-            if cardstack[l] ^ cardstack[r] > goal:
-                l += 1
-            elif cardstack[l] ^ cardstack[r] < goal:
-                r -= 1
-            else:
-                for x in [cardstack[i] for i in list(ii)+[l, r]]:
-                    print(bin(x)[2:].zfill(m))
-                break
+def check(iterable: Iterable[Tuple[int]]):
+    # time complexity: O(n^(k-1)), space complexity: O(n)
+    with alive_bar(int(factorial(n) / (factorial(n - (k-1)) * factorial(k-1))/splits)) as bar:
+        for ii in iterable:  # ii: list of indices
+            bar()
+            goal = reduce(xor, [cardstack[i] for i in ii])
+            l, r = ii[-1]+1, len(cardstack)-1
+            while l < r:
+                if cardstack[l] ^ cardstack[r] > goal:
+                    l += 1
+                elif cardstack[l] ^ cardstack[r] < goal:
+                    r -= 1
+                else:
+                    for x in [cardstack[i] for i in list(ii)+[l, r]]:
+                        print(bin(x)[2:].zfill(m))
+                    break
             
+
+ii = combinations(range(len(cardstack))[:-2], k-1)
