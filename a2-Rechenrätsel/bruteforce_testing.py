d23061ada442594c7b2f93eca885d4726b13f942
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-21 17:32:46 +0200

    fix

--- a2-Rechenrätsel/bruteforce_testing.py
+++ a2-Rechenrätsel/bruteforce_testing.py
@@ -41,7 +41,7 @@ def is_unique(challenge: str, /, progressbar=False, print_nonunique=True) -> boo
     
     while step():
         # test if ops combination matches res and no non-int results
-        combination = ''.join(nums[i//2] if i%2==0 else ops[(i-1)//2] for i in range(length*2+1))
+        combination = '+'+''.join(nums[i//2] if i%2==0 else ops[(i-1)//2] for i in range(length*2+1))
         if search(r'[/*]1', combination):
             continue
         if int(eval(combination)) == int(res):
