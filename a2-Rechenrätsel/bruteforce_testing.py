429434f21ebb4b0a492339f1755c6c0a11178aea
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-21 14:39:43 +0200

    found it.

--- a2-Rechenrätsel/bruteforce_testing.py
+++ a2-Rechenrätsel/bruteforce_testing.py
@@ -21,13 +21,13 @@ def is_unique(challenge: str, /, progressbar=False, print_nonunique=True) -> boo
             for j in range(len(ops)):
                 ops[j] = '+'
         elif ops[i] == '+':
-            ops[i] = '*'
+            ops[i] = '-'
         elif ops[i] == '-':
-            ops[i] == '*'
+            ops[i] = '*'
         elif ops[i] == '*':
-            ops[i] == '/'
+            ops[i] = '/'
         elif ops[i] == '/':
-            ops[i] == '+'
+            ops[i] = '+'
             if i+1<len(ops):
                 step(i+1)
             else:
