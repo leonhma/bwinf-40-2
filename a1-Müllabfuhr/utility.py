a5831ec1ad5aea7ca676807b321ea966d2110482
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-24 00:38:11 +0200

    making tabu raise for debugging

--- a1-Müllabfuhr/utility.py
+++ a1-Müllabfuhr/utility.py
@@ -7,16 +7,20 @@ class TabuList:
     default_tenure: int
     cleanup_freq: int
 
-    def __init__(self, default_tenure: int, /, cleanup_freq=20):
+    def __init__(self, default_tenure: int, /, cleanup_freq=5):
         self.tabu = {}
         self.offset = 0
         self.default_tenure = default_tenure
         self.cleanup_freq = cleanup_freq
 
     def _cleanup(self):
+        raise ValueError
+        to_delete = []
         for k, v in self.tabu.items():
             if v+self.offset <= 0:
-                del self.tabu[k]
+                to_delete.append(k)
+        for k in to_delete:
+            del self.tabu[k]
 
     def add(self, item: Hashable, tenure: int = None):
         self.tabu[item] = (tenure or self.default_tenure)-self.offset
