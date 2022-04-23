7bd4ffd371b5b4dc0f2c26de63562be66e2317c9
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-23 12:55:17 +0200

    removing catch to see detailed errors

--- a1-Müllabfuhr/program.py
+++ a1-Müllabfuhr/program.py
@@ -109,15 +109,12 @@ class CityGraph:
 
 # repl
 while True:
-    try:
-        pth = join(dirname(__file__),
-                        f'beispieldaten/muellabfuhr{input("Bitte die Nummer des Beispiels eingeben [0-9]: ")}.txt')
-        cg = CityGraph._from_bwinf_file(pth)
-        n_days = int(input('Für wieviele Tage soll geplant werden? (5):') or 5)
-        maxlen = 0
-        for i, (len_, p) in zip(range(1, n_days+1), cg.get_paths(n_days)):
-            print(f'Tag {i}: {" -> ".join(map(str, p))}, Gesamtlaenge: {len_}')
-            if len_ > maxlen: maxlen = len_
-        print(f'Maximale Lange einer Tagestour: {maxlen}')
-    except Exception as e:
-        print(e)
+    pth = join(dirname(__file__),
+                    f'beispieldaten/muellabfuhr{input("Bitte die Nummer des Beispiels eingeben [0-9]: ")}.txt')
+    cg = CityGraph._from_bwinf_file(pth)
+    n_days = int(input('Für wieviele Tage soll geplant werden? (5):') or 5)
+    maxlen = 0
+    for i, (len_, p) in zip(range(1, n_days+1), cg.get_paths(n_days)):
+        print(f'Tag {i}: {" -> ".join(map(str, p))}, Gesamtlaenge: {len_}')
+        if len_ > maxlen: maxlen = len_
+    print(f'Maximale Lange einer Tagestour: {maxlen}')
