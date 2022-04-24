334a92789c7d23647565d4a9aa251a962c741811
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-24 15:36:14 +0200

    fix self

--- a1-Müllabfuhr/tabu_optimization.py
+++ a1-Müllabfuhr/tabu_optimization.py
@@ -181,8 +181,8 @@ def MMKCPP_TEE_TabuSearch(G: Dict[int, Dict[int, float]], k: int = 5,
 
     # first make a starting solution
     # all edges in graph + dijkstra between odd connections
-    edges = list(set(frozenset((start, end)) for start in self.vertices for end in self.vertices[start]))
-    odd = [k for k, v in self.vertices.items() if len(v) % 2]
+    edges = list(set(frozenset((start, end)) for start in G for end in G[start]))
+    odd = [k for k, v in G.items() if len(v) % 2]
     for _ in range(0, odd, 2):
         edges.append(list(map(frozenset, edges(dijkstra[odd.pop()][odd.pop()][1]))))
 
