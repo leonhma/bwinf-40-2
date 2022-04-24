4cb3dbea1f1bb3cd4f6dde12335350ec93ba41d4
Leonhard Masche <leonhardmasche@gmail.com>
2022-04-24 23:07:12 +0200

    dropout 99%

--- a1-Müllabfuhr/tabu_optimization.py
+++ a1-Müllabfuhr/tabu_optimization.py
@@ -205,7 +205,7 @@ def MMKCPP_TEE_TabuSearch(G: Dict[int, Dict[int, float]], k: int = 5, maxNOfItsW
 
     while (nOfItsWithoutImprovement < maxNOfItsWithoutImprovement and not
            (maxRunningTime and time() > startTime + maxRunningTime)):
-        
+        print('tabu-it')
         nOfItsWithoutImprovement += 1
         tabuList.tick()
         
